20d95e9b2409970caa36c8ffefc5625ebf19d5b2
Rohith1898 <naidurohith18@gmail.com>
2026-01-07 12:29:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "rohith_terraform"
+    workspaces {
+      name = "tf-cloud-s3"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+}
